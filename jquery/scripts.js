803bef7e1d0b0d18d4feb19c9e5b8f970b4b75d0
Łukasz Soleski <luk.soleski@gmail.com>
2018-10-26 15:09:44 +0200

    jquery ajax post and bug fixes

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -11,57 +11,66 @@ var postsURL = 'https://jsonplaceholder.typicode.com/posts';
 $(document).ready(function () { onReady(); });
 
 function onReady() {
-    GetPostsHandler();
-    //   PostHandlerHandler();
-    HidePostsHandler();
+	GetPostsHandler();
+	//   PostHandlerHandler();
+	HidePostsHandler();
 
-    SendPostHandler();
+	SendPostHandler();
 
 }
 
 function HidePostsHandler() {
-        $('#hidePosts').click(function () {
-            $('#posts').html('');
-        });
+	$('#hidePosts').click(function () {
+		$('#main').html('');
+	});
 }
 
 function GetPostsHandler() {
-    $('#GetPosts').click(function () {
-        $.get(postsURL, function (data, status, xhr) {
-            console.log('status: ' + status);
+	$('#GetPosts').click(function () {
+		$.get(postsURL, function (data, status, xhr) {
+			console.log('status: ' + status);
             console.log('xhr: ' + xhr);
-            $(data).each(function (index, el) {
-               appendPost(el);
-            });
-        });
-    });
+            console.log(data);
+            let i = 0;
+			$(data).each(function (index, el) {
+                
+                appendPost(el);
+                i += 1;
+              
+			});
+		});
+	});
 }
 
 
 function SendPostHandler(){
-    $('#send').click(function(){
-        let id = $('userId').value;
-        let body = $('body').value;
-        let title = $('title').value;
 
-        let obj = {'userId': userId, 'body':body,'title':title};
+	$('#send').click(function(event){
+        event.preventDefault();
 
+		let id = $('#userId').val();
+		let body = $('#body').val();
+		let title = $('#title').val();
 
-        $.Post(postsURL,JSON.stringify(obj),function callback(data,status,xhr){
-                if(status === 201){
-                    $(data).each(function(index,el){appendPost(el);});
-                }
-        }, );
-
-    });
-
-    
+        let obj = {'userId': id, 'body':body,'title':title};
+        
+		$.post(postsURL,obj,function(data,status,xhr){
+			if(xhr.status === 201){
+                appendPost(data);
+            }else{
+                alert('server status returned:\n' + status);
+                console.log('another status : ' + JSON.stringify(status));
+                console.log('xhr: ' + JSON.stringify(xhr));
+            } 
+		},'json');
+	});
 }   
-
+  
 function appendPost(el){
-    $('#posts').children('tbody').append('<tr>' +
+	$('#main').append('<tr>' +
     '<td>' + el.id + '</td>' +
     '<td>' + el.title + '</td>' +
     '<td>' + el.body + '</td>' +
     '</tr>');
+  
 }
